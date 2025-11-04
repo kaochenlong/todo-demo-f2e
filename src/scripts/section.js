c8a8cc1958673c635e177a5ad8825356a46389ad
Eddie Kao <eddie@5xcampus.com>
2025-11-04 11:11:18 +0800

    feat(auth): add user login flow with JWT token persistence

--- src/scripts/section.js
+++ src/scripts/section.js
@@ -7,6 +7,33 @@ function changeSection() {
     nickname: "",
     password: "",
 
+    async doLogin() {
+      const { email, password } = this
+
+      if (email != "" && password != "") {
+        // API
+        const userData = {
+          user: {
+            email,
+            password,
+          },
+        }
+
+        try {
+          const resp = await axios.post("https://todoo.5xcamp.us/users/sign_in", userData)
+          const token = resp.headers.authorization
+
+          if (token) {
+            localStorage.setItem("todoToken", token)
+          }
+          this.resetForm()
+          this.gotoTask()
+        } catch (err) {
+          console.log(err)
+        }
+      }
+    },
+
     async doSignUp() {
       const { email, nickname, password } = this
 
@@ -20,12 +47,19 @@ function changeSection() {
         }
 
         try {
-          const resp = await axios.post("https://todoo.5xcamp.us/users", userData)
-          console.log(resp)
+          await axios.post("https://todoo.5xcamp.us/users", userData)
+          this.resetForm()
+          this.gotoLogin()
         } catch (err) {
           alert(err.response.data.message)
         }
       }
+    },
+
+    resetForm() {
+      this.email = ""
+      this.password = ""
+      this.nickname = ""
     },
 
     gotoLogin() {
@@ -34,7 +68,9 @@ function changeSection() {
     gotoSignUp() {
       this.change_section = "signup"
     },
-
+    gotoTask() {
+      this.change_section = "task"
+    },
     showLogin() {
       return this.change_section == "login"
     },
